29fc135fce17c6f2b90ac41399f82276d48e7fa4
Bardia Moshiri <fakeshell@bardia.tech>
2023-01-14 01:20:50 -0500

    set a variable for zip name to use at the end of the build and clean up

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-whoami
 echo "Downloading the image"
 wget -nv https://github.com/droidian-images/droidian/releases/download/droidian%2Fbookworm%2F24/droidian-OFFICIAL-phosh-phone-rootfs-api29-arm64-24_20220804.zip
+
 echo "Readying up the image"
 unzip droidian-OFFICIAL-phosh-phone-rootfs-api29-arm64-24_20220804.zip
 mkdir ./rootfs/ -p
@@ -13,15 +13,20 @@ DEVICE_ROOTFS=$(losetup -f)
 ROOTFS_PATH="./rootfs/"
 losetup ${DEVICE_ROOTFS} ./data/rootfs.img
 mount ${DEVICE_ROOTFS} ${ROOTFS_PATH}
+
 echo "Installing dependencies"
 apt update
 apt install qemu-user-static img2simg -y
+
 echo "Downloading adaptation"
 wget -nv https://mirror.bardia.tech/exynos9810/pool/main/adaptation-droidian-exynos9810_0.0.0+git20230110092736.88cb61b.main_all.deb -P ./rootfs/
 wget -nv https://mirror.bardia.tech/exynos9810/pool/main/adaptation-exynos9810-configs_0.0.0+git20230110092736.88cb61b.main_all.deb -P ./rootfs/
+wget -nv https://mirror.bardia.tech/exynos9810/pool/main/1lib11-xcb1_1.7.5-arm64.deb -p ./rootfs/
+wget -nv https://mirror.bardia.tech/exynos9810/pool/main/2libwlroots7c_0.12.0-1hybrismobian6+git20220410132816.870098d.bookworm.hwc2.fallback_arm64.deb -p ./rootfs/
 cp /usr/bin/qemu-aarch64-static ./rootfs/usr/bin/
+
 echo "Applying adaptation"
-chroot ./rootfs/ qemu-aarch64-static /bin/bash -c 'export PATH="$PATH:/usr/bin:/usr/sbin:/bin:/sbin" && rm -f /etc/systemd/system/dbus-org.bluez.service && systemctl mask systemd-resolved systemd-timesyncd upower bluetooth && dpkg -i /*.deb && rm /*.deb /deb -rf && systemctl enable apt-fix samsung-hwc epoch altresolv upoweralt bluetoothalt batman'
+chroot ./rootfs/ qemu-aarch64-static /bin/bash -c 'export PATH="$PATH:/usr/bin:/usr/sbin:/bin:/sbin" && rm -f /etc/systemd/system/dbus-org.bluez.service && systemctl mask systemd-resolved systemd-timesyncd upower bluetooth && dpkg -i /*.deb && rm /*.deb /deb -rf && systemctl enable apt-fix samsung-hwc epoch altresolv upoweralt bluetoothalt batman && apt-mark hold phosh && apt-mark hold phoc && apt-mark hold libwlroots7c && apt-mark hold lib11-xcb1'
 rm ./rootfs/usr/bin/qemu-aarch64-static
 
 ROOTFS_SIZE=$(du -sm ${ROOTFS_PATH} | awk '{ print $1 }')
@@ -104,26 +109,26 @@ if [ ! -d "android-image-flashing-template" ]; then
     apt install git -y
     git clone https://github.com/sexynos/android-image-flashing-template
 fi
-echo ${WORK_DIR}
-echo ${ZIP_NAME}
 ls ${WORK_DIR}
 ls ./
 pwd
+ls ~/
 mkdir -p ${WORK_DIR}/target/data/
 rm -r android-image-flashing-template/template/data
 cp -R android-image-flashing-template/template/* ${WORK_DIR}/target/
 mv ${WORK_DIR}/userdata.img ${WORK_DIR}/target/data/userdata.img
 
-apt update
-apt install wget -y
-wget https://github.com/Sexynos/droidian-kernel-samsung-exynos9810/releases/download/star2lte/boot-star2lte.img
-wget https://github.com/Sexynos/droidian-kernel-samsung-exynos9810/releases/download/star2lte/recovery.img
+echo "Downloading flashable images"
+wget -nv https://github.com/Sexynos/droidian-kernel-samsung-exynos9810/releases/download/star2lte/boot-star2lte.img
+wget -nv https://github.com/Sexynos/droidian-kernel-samsung-exynos9810/releases/download/star2lte/recovery.img
 cp ./boot-star2lte.img ${WORK_DIR}/target/data/boot.img
 cp ./recovery.img ${WORK_DIR}/target/data/recovery.img
 
 # generate zip
 echo "Generating zip"
-mkdir ../../out/
-(cd ${WORK_DIR}/target ; zip -r9 ../../out/rootfs.zip * -x .git README.md *placeholder)
+mkdir out/ -p
+DATE=$(date +'%Y%m%d')
+ZIP_NAME="droidian-UNOFFICIAL-phosh-phone-samsung_star2lte-api29-arm64-nightly_$DATE.zip"
+(cd ${WORK_DIR}/target ; zip -r9 ../../out/$ZIP_NAME * -x .git README.md *placeholder)
 
 echo "done."
